e59d72e116089d9cb99c0f67748688c6a81375c0
Pierre Amelot <pierre.p.amelot@gmail.com>
2023-12-17 23:41:26 +0100

    Add session info endpoint
    * List paginated events for the session
    * Provide stats on all the events of the session
    * Add stats about all the events to compare this session with others
        and to know what has and hasn't been clicked on.

--- src/queries/analytics/sessions/getSessionsForPierre.ts
+++ src/queries/analytics/sessions/getSessionsForPierre.ts
@@ -1,4 +1,4 @@
-import { Prisma, Session } from '@prisma/client';
+import { EventData, Prisma, Session, WebsiteEvent } from '@prisma/client';
 import prisma from 'lib/prisma';
 import { type FilterResult, type SearchFilter } from 'lib/types';
 
@@ -13,17 +13,37 @@ interface SessionFilter {
   city?: string;
 }
 
-interface SessionResult extends Omit<Session, 'subdivision1'> {
+interface ExtendedSession extends Omit<Session, 'subdivision1'> {
   region: string;
   pageViews: number;
   latestEventAt: Date;
 }
 
 export interface SessionSearchFilter extends Omit<SearchFilter, 'query'>, SessionFilter {}
+export interface SessionEventsSearchFilter extends Omit<SearchFilter, 'query'> {}
+
+interface PrismaExtendedSession extends Session {
+  websiteEvent: {
+    createdAt: Date;
+  }[];
+  _count: {
+    websiteEvent: number;
+  };
+}
+function renameExtendedSession(prismaExtendedSession: PrismaExtendedSession) {
+  const { _count, websiteEvent, subdivision1, ...rest } = prismaExtendedSession;
+
+  return {
+    ...rest,
+    pageViews: _count.websiteEvent,
+    latestEventAt: websiteEvent?.[0]?.createdAt,
+    region: subdivision1,
+  };
+}
 
 export async function getSessionsByWebsiteId(
   filters?: SessionSearchFilter,
-): Promise<FilterResult<SessionResult>> {
+): Promise<FilterResult<ExtendedSession>> {
   const { websiteId, os, browser, device, country, language, region, city, ...rest } =
     filters || {};
 
@@ -55,14 +75,130 @@ export async function getSessionsByWebsiteId(
     ...pageFilters,
   });
 
-  const sessionsRenamed = sessions.map(({ _count, websiteEvent, subdivision1, ...rest }) => ({
-    ...rest,
-    latestEventAt: websiteEvent?.[0]?.createdAt,
-    pageViews: _count.websiteEvent,
-    region: subdivision1,
-  }));
+  const sessionsRenamed = sessions.map(prismaExtendedSession =>
+    renameExtendedSession(prismaExtendedSession),
+  );
 
   const count = await prisma.client.session.count({ where });
 
   return { data: sessionsRenamed, count, ...getParameters };
+}
+
+async function getSessionInfo(sessionId: string): Promise<ExtendedSession> {
+  const session = await prisma.client.session.findUnique({
+    where: { id: sessionId },
+    include: {
+      _count: {
+        select: {
+          websiteEvent: { where: { eventType: 1 } },
+        },
+      },
+      websiteEvent: { take: 1, orderBy: { createdAt: 'desc' }, select: { createdAt: true } },
+    },
+  });
+
+  return renameExtendedSession(session);
+}
+
+type EventStats = {
+  eventNames: ({ _count: number } & Pick<WebsiteEvent, 'eventName' | 'eventType'>)[];
+  eventData: ({ _count: number } & Pick<
+    EventData,
+    'eventKey' | 'stringValue' | 'dataType' | 'numberValue' | 'dateValue'
+  >)[];
+};
+
+async function getSessionEvents(
+  sessionId: string,
+  filters?: SessionEventsSearchFilter,
+): Promise<{
+  events: FilterResult<Omit<WebsiteEvent, 'id' | 'websiteId' | 'sessionId'>>;
+  eventStats: EventStats;
+  allEventStats: EventStats;
+}> {
+  const [pageFilters, getParameters] = prisma.getPageFilters(filters);
+
+  const where: Prisma.WebsiteEventWhereInput = {
+    sessionId,
+  };
+
+  const [sessionEvents, eventNamesStats, eventDataStats, allEventNamesStats, allEventDataStats] =
+    await Promise.all([
+      prisma.client.websiteEvent.findMany({
+        where,
+        select: {
+          createdAt: true,
+          eventName: true,
+          eventType: true,
+          pageTitle: true,
+          urlPath: true,
+          urlQuery: true,
+          referrerDomain: true,
+          referrerPath: true,
+          referrerQuery: true,
+          eventData: {
+            orderBy: { createdAt: 'desc' },
+            select: {
+              createdAt: true,
+              dataType: true,
+              dateValue: true,
+              eventKey: true,
+              numberValue: true,
+              stringValue: true,
+            },
+          },
+        },
+        ...pageFilters,
+      }),
+      prisma.client.websiteEvent.groupBy({
+        where,
+        by: ['eventName', 'eventType'],
+        _count: true,
+      }),
+      prisma.client.eventData.groupBy({
+        where: { websiteEvent: { sessionId } },
+        by: ['eventKey', 'stringValue', 'dataType', 'numberValue', 'dateValue'],
+        _count: true,
+      }),
+      // Add stats about all the events to compare this session with others and
+      // to know what has and hasn't been clicked on.
+      prisma.client.websiteEvent.groupBy({
+        by: ['eventName', 'eventType'],
+        _count: true,
+      }),
+      prisma.client.eventData.groupBy({
+        by: ['eventKey', 'stringValue', 'dataType', 'numberValue', 'dateValue'],
+        _count: true,
+      }),
+    ]);
+
+  const count = await prisma.client.websiteEvent.count({ where });
+
+  return {
+    eventStats: {
+      eventNames: eventNamesStats,
+      eventData: eventDataStats,
+    },
+    allEventStats: {
+      eventNames: allEventNamesStats,
+      eventData: allEventDataStats,
+    },
+    events: {
+      data: sessionEvents,
+      count,
+      ...getParameters,
+    },
+  };
+}
+
+export async function getSessionInfoAndEvents(
+  sessionId: string,
+  filters?: SessionEventsSearchFilter,
+) {
+  const [sessionInfo, events] = await Promise.all([
+    getSessionInfo(sessionId),
+    getSessionEvents(sessionId, filters),
+  ]);
+
+  return { info: sessionInfo, ...events };
 }
